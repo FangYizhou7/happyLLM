28c415a7fdc82ebf7444e97b246b4a8a79191214
FangYizhou7 <109298973+FangYizhou7@users.noreply.github.com>
2026-01-10 11:58:43 +0800

    reshape for broadcast

--- model/attention.py
+++ model/attention.py
@@ -36,3 +36,10 @@ def reshape_for_broadcast(freqs_cis:torch.Tensor,x:torch.Tensor):
 
     # 断言确保1在x的维度范围内
     assert 0<=1<ndim
+
+    assert freqs_cis.shape==(x.shape[1],x.shape[-1])
+
+    # 构造一个新的形状，除了第二维和最后一维，其他维度都为1，这样做是为了能够将freqs_cis与x进行广播操作
+    shape=[d if i==1 or i==ndim-1 else 1 for i,d in enumerate(x.shape)]
+
+    return freqs_cis.view(shape)
