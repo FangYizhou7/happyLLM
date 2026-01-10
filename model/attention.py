bb8a94ab38d959b37a1cf29e6fdb87423d5db8d7
FangYizhou7 <109298973+FangYizhou7@users.noreply.github.com>
2026-01-10 17:50:35 +0800

    Attention

--- model/attention.py
+++ model/attention.py
@@ -1,4 +1,49 @@
+from typing import Tuple
+
 import torch
+from torch import nn
+
+from config.ModelConfig import ModelConfig
+
+
+class Attention(nn.Module):
+    def __init__(self,args:ModelConfig):
+        super().__init__()
+        self.n_kv_heads=args.n_heads if args.n_kv_heads is None else args.n_kv_heads
+
+        #确保总头数可以被键值头数整除
+        assert args.n_heads % args.n_kv_heads == 0
+
+        #模型并行处理大小，默认为1
+        model_parallel_size=1
+        # 本地计算头数 等于总头数除以模型并行处理大小
+        self.n_local_heads=args.n_heads // model_parallel_size
+
+        #本地键值头数 等于总键值头数除以模型并行处理大小
+        self.n_local_kv_heads=self.n_kv_heads // model_parallel_size
+
+        #重复次数 用于扩展键和值的尺寸
+        self.n_rep=self.n_local_heads //  self.n_local_kv_heads
+
+        #每个头的维度 等于模型维度除以头的总数
+        self.head_dim=args.dim// args.n_heads
+
+
+        #定义权重矩阵
+        self.wq=nn.Linear(args.dim,args.n_heads*self.head_dim,bias=False)
+        self.wk=nn.Linear(args.dim,self.n_kv_heads*self.head_dim,bias=False)
+        self.wv=nn.Linear(args.dim,self.n_kv_heads*self.head_dim,bias=False)
+
+        #输出权重矩阵
+        self.wo=nn.Linear(args.n_heads*self.head_dim,args.dim,bias=False)
+
+        #定义dropout
+        self.attn_dropout=nn.Dropout(args.dropout)
+        self.resid_dropout=nn.Dropout(args.dropout)
+
+
+
+
 
 # 实现匹配多头注意力
 def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
@@ -17,7 +62,8 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
     )
 
 
-# 实现旋转嵌入
+
+
 def precompute_freqs_cis(dim:int,end:int,theta:float=10000.0):
     freqs=1.0/(theta**(torch.arange(0,dim,2)[:(dim//2)].float()/dim))
     t=torch.arange(end,device=freqs.device)
@@ -43,3 +89,31 @@ def reshape_for_broadcast(freqs_cis:torch.Tensor,x:torch.Tensor):
     shape=[d if i==1 or i==ndim-1 else 1 for i,d in enumerate(x.shape)]
 
     return freqs_cis.view(shape)
+
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cos: torch.Tensor,
+    freqs_sin: torch.Tensor
+) -> Tuple[torch.Tensor, torch.Tensor]:
+
+    # 将查询和键张量转换为浮点数，并重塑形状以分离实部和虚部
+    xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
+    xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
+
+    # 重新塑形频率张量以进行广播
+    freqs_cos = reshape_for_broadcast(freqs_cos, xq_r)
+    freqs_sin = reshape_for_broadcast(freqs_sin, xq_r)
+
+    # 应用旋转，分别计算旋转后的实部和虚部
+    xq_out_r = xq_r * freqs_cos - xq_i * freqs_sin
+    xq_out_i = xq_r * freqs_sin + xq_i * freqs_cos
+    xk_out_r = xk_r * freqs_cos - xk_i * freqs_sin
+    xk_out_i = xk_r * freqs_sin + xk_i * freqs_cos
+
+    # 将最后两个维度合并，并还原为原始张量的形状
+    xq_out = torch.stack([xq_out_r, xq_out_i], dim=-1).flatten(3)
+    xk_out = torch.stack([xk_out_r, xk_out_i], dim=-1).flatten(3)
+
+    return xq_out.type_as(xq), xk_out.type_as(xk)
+
